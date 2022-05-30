dd00c28e1bf585716636e035a46837289846710d
matthewjrogers <matthew.rogers09@gmail.com>
2022-05-30 09:18:52 -0400

    docs

--- R/write_db_targ.R
+++ R/write_db_targ.R
@@ -1,3 +1,12 @@
+#' Write a data frame to a database using a `db_targ`
+#'
+#' @param con A database connection
+#' @param df A data frame
+#' @param targ A `db_targ` object
+#' @param ... Arbitrary arguments to pass to `DBI::dbWriteTable`
+#'
+#' @export
+#'
 write_db_targ <- function(con, df, targ, ...){
   validate_targets_dbtable(targ)
   
