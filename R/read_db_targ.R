151f98e3a635c431c0fde2007a5f1c66e6436d3c
matthewjrogers <matthew.rogers09@gmail.com>
2022-05-30 09:08:40 -0400

    docs

--- R/read_db_targ.R
+++ R/read_db_targ.R
@@ -1,3 +1,11 @@
+#' Read a `db_targ` table
+#'
+#' @param con A database connection
+#' @param targ A `db_targ` object
+#'
+#' @return A data.frame
+#' @export
+#'
 read_db_targ <- function(con, targ){
   validate_targets_dbtable(targ)
   
