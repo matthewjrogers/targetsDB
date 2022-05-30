2daf5b77a19f240787b16db5cdaf1031f902beb8
matthewjrogers <matthew.rogers09@gmail.com>
2022-05-30 09:22:14 -0400

    docs

--- R/save_db_targ.R
+++ R/save_db_targ.R
@@ -1,3 +1,11 @@
+#' Save a `db_targ` object. Convenience wrapper for base `saveRDS`
+#'
+#' @param targ A `db_targ` object
+#' @param file_path File to save the `db_targ` object to. Must be a .rds file
+#'
+#' @return file_path, invisibly
+#' @export
+#'
 save_db_targ <- function(targ, file_path){
   validate_targets_dbtable(targ)
   
